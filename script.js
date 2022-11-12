e4b26306dba405eb8e530752a63cb2fbe1529482
Azat <bayanov.azot@gmail.com>
2022-11-12 15:29:46 +0300

    Add monochrome drawing function

--- script.js
+++ script.js
@@ -4,9 +4,51 @@ function createPixelGrid(n) {
     const gridElement = document.createElement("div");
     gridElement.classList.add("element");
     for (let i = 0; i < n**2; i++) {
-        gridContainer.appendChild(gridElement.cloneNode());
+        let cell = gridElement.cloneNode();
+        cell.addEventListener("mousedown", changeColor);
+        gridContainer.appendChild(cell);
     }
 }
 
+function changeColor() { 
+    this.style["background-color"] = "black";
+}
+
+function startDrawing() {
+    one.addEventListener("mouseover", print);
+    two.addEventListener("mouseover", print);
+}
+
+function stopDrawing() {
+    one.removeEventListener("mouseover", print);
+    two.removeEventListener("mouseover", print);
+
+}
+
 createPixelGrid(16);
+
+const cells = document.getElementsByClassName("element");
+
+window.addEventListener("mousedown", function() {
+    Array.from(cells).forEach(cell => {
+        cell.addEventListener("mouseover", changeColor);
+    });
+});
+
+window.addEventListener("mouseup", function() {
+    Array.from(cells).forEach(cell => {
+        cell.removeEventListener("mouseover", changeColor);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
 
