b32fa23c863a35256c5a2e8294f72df3f21176b0
Azat <bayanov.azot@gmail.com>
2022-11-12 20:23:46 +0300

    Add color picker and make grid elements undraggable

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ function createPixelGrid(n) {
     gridContainer.style["grid-template-columns"] = `repeat(${n}, auto)`;
     const gridElement = document.createElement("div");
     gridElement.classList.add("element");
+    gridElement.classList.add("noselect");
+    gridElement.setAttribute("draggable", "false");
+
     for (let i = 0; i < n**2; i++) {
         let cell = gridElement.cloneNode();
         cell.addEventListener("mousedown", changeColor);
@@ -28,8 +31,19 @@ function stopDrawing() {
 createPixelGrid(16);
 
 let color = "black";
+let userColor = "black";
+
 const eraser = document.getElementById("eraser");
 eraser.addEventListener("click", () => color = "white");
+
+const colorPicker = document.getElementById("colorPicker");
+colorPicker.addEventListener("change", e =>  {
+    userColor = e.target.value;
+    color = userColor;
+});
+
+const pencil = document.getElementById("pencil");
+pencil.addEventListener("click", () => color = userColor);
 
 const cells = document.getElementsByClassName("element");
 
