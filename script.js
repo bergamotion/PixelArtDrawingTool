de67e6ef9ccf233304caa7462eea18529de0387b
Azat <bayanov.azot@gmail.com>
2022-11-13 15:49:48 +0300

    Add rainbow mode

--- script.js
+++ script.js
@@ -22,8 +22,21 @@ function createPixelGrid(n) {
     }
 }
 
-function changeColor() { 
-    this.style["background-color"] = color;
+function changeColor() {
+    if (rainbowMode) {
+        this.style["background-color"] = `hsl(${color}, 100%, 50%)`;
+        color = (color + 10) % 360;
+    } else {
+        this.style["background-color"] = color;
+    }
+}
+
+function toggleButtonStyle() {
+    for (let btn of controlButtons) {
+        if (btn.classList.contains("activeButton")) {
+            btn.classList.toggle("activeButton");
+        }
+    }
 }
 
 createPixelGrid(16);
@@ -31,46 +44,47 @@ createPixelGrid(16);
 // Global color variables
 let color = "black";
 let userColor = "black";
+let rainbowMode = false;
 
 // Eraser Button
 const eraser = document.getElementById("eraser");
 eraser.addEventListener("click", () => {
+    rainbowMode = false;
     color = "white";
-    for (let btn of controlButtons) {
-        if (btn.classList.contains("activeButton")) {
-            btn.classList.toggle("activeButton");
-        }
-    }
+    toggleButtonStyle();
     eraser.classList.toggle("activeButton");
 });
  
 // Color Picker
 const colorPicker = document.getElementById("colorPicker");
 colorPicker.addEventListener("change", e =>  {
+    rainbowMode = false;
     userColor = e.target.value;
     color = userColor;
-    for (let btn of controlButtons) {
-        if (btn.classList.contains("activeButton")) {
-            btn.classList.toggle("activeButton");
-        }
-    }
+    toggleButtonStyle();
     pencil.classList.toggle("activeButton");
 });
 
 // Pencil Button
 const pencil = document.getElementById("pencil");
 pencil.addEventListener("click", () => {
+    rainbowMode = false;
     color = userColor;
-    for (let btn of controlButtons) {
-        if (btn.classList.contains("activeButton")) {
-            btn.classList.toggle("activeButton");
-        }
-    }
+    toggleButtonStyle();
     pencil.classList.toggle("activeButton");
 });
 
+// Rainbow mode Button
+const rainbow = document.getElementById("rainbow");
+rainbow.addEventListener("click", () => {
+    toggleButtonStyle();
+    rainbow.classList.toggle("activeButton");
+    rainbowMode = true;
+    color = 0;
+});
+
 // Global buttons variables
-const controlButtons = [eraser, pencil];
+const controlButtons = [eraser, pencil, rainbow];
 let activeButton = pencil;
 
 // Grid Size Slider
